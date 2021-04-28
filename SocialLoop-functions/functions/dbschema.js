92144fe920b19bade6134d197348c156ae381b04
KKoder1991 <konondlovu@icloud.com>
2021-04-28 19:44:29 +0200

    Add two more items for DB

--- SocialLoop-functions/functions/dbschema.js
+++ SocialLoop-functions/functions/dbschema.js
@@ -3,7 +3,9 @@ let db = {
         {
             loopHandle: 'user',
             body: 'This is a comment by the looper.',
-            createdAt: "2021-04-28T17:34:03.578Z"
+            createdAt: "2021-04-28T17:34:03.578Z",
+            likeCount: 5,
+            commentCount: 2
         }
     ]
 }
